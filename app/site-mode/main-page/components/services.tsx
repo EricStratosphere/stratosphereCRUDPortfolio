009c8721cbeea4078ba665b27a38582394e7b02e
EricStratosphere <140722924+EricStratosphere@users.noreply.github.com>
2026-01-16 01:51:32 +0800

    Add styling to text under service item icons.

--- app/site-mode/main-page/components/services.tsx
+++ app/site-mode/main-page/components/services.tsx
@@ -11,21 +11,26 @@ export default function Services(){
 
                 <div className={styles['services-box']}>
                     <div className={styles['service-item']}>
-                        <a className={styles['service-button']}href="">
-                            <Image className={styles['service-img']}src='/favicon.ico' alt='icon' width={0} height={0}></Image>
-                        </a>
+                        <button className={styles['service-button']}>
+                            <Image className={styles['service-img']}src='/Vector(1).svg' alt='icon' width={0} height={0}></Image>
+                        </button>
+                        Book cover art
                     </div>
 
                     <div className={styles['service-item']}>
-                        <a className={styles['service-button']}href="">
-                            <Image className={styles['service-img']}src='/favicon.ico' alt='icon' width={0} height={0}></Image>
-                        </a>
+                        <button className={styles['service-button']}>
+                            <Image className={styles['service-img']}src='/Vector.svg' alt='icon' width={0} height={0}></Image>
+                        </button>
+                        Illustrations
                     </div>
+
                     <div className={styles['service-item']}>
-                        <a className={styles['service-button']}href="">
-                            <Image className={styles['service-img']}src='/favicon.ico' alt='icon' width={0} height={0}></Image>
-                        </a>
+                        <button className={styles['service-button']}>
+                            <Image className={styles['service-img']}src='/Vector(2).svg' alt='icon' width={0} height={0}></Image>
+                        </button>
+                        Music Cover art
                     </div>
+                   
                 </div>
             </div>
         </>
