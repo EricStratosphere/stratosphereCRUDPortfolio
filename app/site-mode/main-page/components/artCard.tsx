f9decfc55833ec3d01077aa6e3ff9b974ab33bba
EricStratosphere <140722924+EricStratosphere@users.noreply.github.com>
2026-01-16 22:49:51 +0800

    Add text overlay on hover to art card

---
+++ app/site-mode/main-page/components/artCard.tsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import styles from '../styles/art-collage.module.css'
+interface ArtCardProps{
+    imgUrl : string,
+    artworkName : string,
+}
+
+export default function ArtCard({imgUrl, artworkName} : ArtCardProps){
+    
+    return(
+        <>
+
+        </>
+    );
+}
