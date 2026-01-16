bf14145a31c1c4b8b632028ab27b450292efd8f1
EricStratosphere <140722924+EricStratosphere@users.noreply.github.com>
2026-01-16 16:16:47 +0800

    Add styling to art-collage

--- app/site-mode/main-page/components/art-collage.tsx
+++ app/site-mode/main-page/components/art-collage.tsx
@@ -1,10 +1,33 @@
 "use server"
 
-
+import styles from '../styles/art-collage.module.css'
+import Image from 'next/image';
 export default async function ArtCollage(){
     return(
         <>
-            This is the art collage.  
+            <div className={styles['art-collage']}>
+                <div className={styles['collage-text']}>
+                    Works
+                </div>
+                <div className={styles['artworks']}>
+                    <button>
+                        <Image src="/Howl.png" alt="artwork" width={0} height={0} className={styles['artwork']}></Image>            
+                    </button>
+                    <button>
+                        <Image src="/Arcane.png" alt="artwork" width={0} height={0} className={styles['artwork']}></Image>            
+                    </button>
+                    <button>
+                        <Image src="/Atelier.png" alt="artwork" width={0} height={0} className={styles['artwork']}></Image>            
+                    </button>
+                    <button>
+                        <Image src="/Dreamnotfound.jpg" alt="artwork" width={0} height={0} className={styles['artwork']}></Image>            
+                    </button>
+                    <button>
+                        <Image src="/songOfAchilles.png" alt="artwork" width={0} height={0} className={styles['artwork']}></Image>            
+                    </button>
+
+                </div>
+            </div>
         </>
     );
 }
